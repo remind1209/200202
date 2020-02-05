e4dc803ec0f84071274652fbe0eb166106920e88
evan <252286490@qq.com>
2020-02-06 00:34:36 +0800

    222

---
+++ src/components/headerNav/index.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+import './index.less'
+
+class HeaderNav extends Component {
+    render(){
+        return (
+            <div className="header-nav">
+                header-nav
+            </div>
+        )
+    }
+}
+
+export default HeaderNav
--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,12 +1,24 @@
 import React ,{Component} from 'react';
+import LeftNav from "../../components/left-nav";
+import HeaderNav from "../../components/headerNav";
+import { Layout } from 'antd';
+import './index.less'
+const { Footer, Sider, Content } = Layout;
 
 
 class Admin extends Component {
     render(){
         return (
-            <div className="div">
-                <p className="p">123123</p>
-            </div>
+            <Layout style={{height:'100%'}}>
+                <Sider>
+                    <LeftNav/>
+                </Sider>
+                <Layout>
+                    <HeaderNav/>
+                    <Content style={{backgroundColor:'#fff'}}>Content</Content>
+                    <Footer style={{textAlign:'center',fontSize:"25px"}}>版权所有 Create 2020 -------From:Mr.shen</Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
